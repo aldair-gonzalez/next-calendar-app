0437a7523b4c1f8971f1cc72c27b8d802b531419
Aldair Gonzalez <aldairgome97@gmail.com>
2024-02-09 17:50:55 -0600

    Ajustando layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} w-screen h-screen p-10 flex flex-col gap-10 items-center justify-center`}>{children}</body>
     </html>
   );
 }
